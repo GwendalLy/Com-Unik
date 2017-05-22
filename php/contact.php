d974ed73461d68bed3f9cfe3ead5344ef4d289c6
marinebelin <marinebelin@free.fr>
2017-05-22 15:26:06 +0200

    ajout du php

--- php/contact.php
+++ php/contact.php
@@ -1,30 +1,31 @@
 <?php
 
 
- if (isset($_GET['name']) && isset($_GET['email']) && isset($_GET['mess'])){
+  if (isset($_GET['nom']) && isset($_GET['email']) && isset($_GET['message'])){
 
-        echo '<p>'.'Merci '.$_GET['name'].' !'.'</p>';
+        echo '<p>'.'Merci '.$_GET['nom'].' !'.'</p>';
 
-    } else {
+  } 
 
-           echo '
-                <form action="test.php" method="get">
-                    <div>
-                        <label for="nom">Nom :</label>
-                        <input type="text" id="nom" name="name" required/>
-                    </div>
-                    <div>
-                        <label for="courriel">Courriel :</label>
-                        <input type="email" id="courriel"  name="email" required/>
-                    </div>
-                    <div>
-                        <label for="message">Message :</label>
-                        <textarea id="message" name="mess" required></textarea>
-                    </div>
 
-                   <button type="submit" > SEND </button>
-                </form> ';
+  if (empty($_GET["nom"])) {
 
-    }
+      echo "Name is required <br>";
+    
+  }
 
+  if (empty($_GET["email"])) {
+
+      echo "Email is required <br>";
+    
+  }
+
+
+  if (empty($_GET["message"])) {
+
+      echo "Message is required <br>";
+    
+  }
+
+  
 ?>
