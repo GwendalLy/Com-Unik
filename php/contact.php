5335c3bf770004843606735924645180b13d7865
marinebelin <marinebelin@free.fr>
2017-05-23 08:41:43 +0200

    php

--- php/contact.php
+++ php/contact.php
@@ -1,31 +1,54 @@
 <?php
 
 
-  if (isset($_GET['nom']) && isset($_GET['email']) && isset($_GET['message'])){
+    $error = array( );
 
-        echo '<p>'.'Merci '.$_GET['nom'].' !'.'</p>';
-
-  } 
+    if (empty($_GET["nom"])) {
 
 
-  if (empty($_GET["nom"])) {
+       $error['name'] = false; 
 
-      echo "Name is required <br>";
-    
-  }
+      
+    }else{
 
-  if (empty($_GET["email"])) {
-
-      echo "Email is required <br>";
-    
-  }
+      $error['name'] = true;
 
 
-  if (empty($_GET["message"])) {
+    }
 
-      echo "Message is required <br>";
-    
-  }
+    if (filter_var(($_GET["email"]), FILTER_VALIDATE_EMAIL)) {
 
+      $error['email'] = true;
+
+      
+    }else{
+
+      $error['email'] = false;
+
+
+    }
+
+
+    if (empty($_GET["message"])) {
+
+
+      $error['message'] = false;
+
+
+      
+    }else{
+
+      $error['message'] = true;
+
+
+    }
+
+    if ($error['name'] = true && $error['email'] = true && $error['message'] = true){
+      
+    } 
+
+
+    echo json_encode($error);
   
+
 ?>
